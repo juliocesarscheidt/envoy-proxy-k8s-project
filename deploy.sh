33b2bf8e6299d2b51ae9a148384b30768e052392
julio-cesar-development <julio.scheidt96@gmail.com>
2020-07-23 01:47:00 -0300

    Several improvements and changes - Add CI - Change ingress to traefik - Improve K8S yaml files
    Signed-off-by: julio-cesar-development <julio.scheidt96@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -2,40 +2,16 @@
 
 set -e
 
-if [ -z "$(which kubectl)" -o -z "$(which helm)" ]; then
-  echo "You must install kubectl and helm tools in order to execute this script"
+if [ -z "$(which kubectl)" ]; then
+  echo "You must install kubectl in order to execute this script"
   exit 1
 fi
 
-# namespaces
-kubectl apply -f ./namespaces.yaml
+# Envoy deployment
+kubectl apply -f ./traefik-ingress-controller.yaml
 
-# limit range
-kubectl apply -f ./limit-range.yaml
+# wait for Envoy
+sleep 30
 
-# API objects
-kubectl apply -f ./api-deployment.yaml
-kubectl apply -f ./api-svc.yaml
-
-# Envoy objects
-kubectl apply -f ./envoy-deployment.yaml
-kubectl apply -f ./envoy-svc.yaml
-
-# Nginx ingress
-INGRESS_INSTALLED=$(helm ls --all -n ingress-nginx | grep deployed)
-if [ -z "${INGRESS_INSTALLED}" ]; then
-  helm repo add nginx-stable https://helm.nginx.com/stable
-  helm repo update
-  helm install ingress-nginx \
-    -n ingress-nginx \
-    --set controller.watchNamespace=envoy \
-    --set controller.kind=deployment \
-    --set controller.service.name=ingress-nginx \
-    --set controller.service.httpPort.enable=true \
-    --set controller.service.httpPort.port=80 \
-    nginx-stable/nginx-ingress
-  # kubectl get pod,deploy,svc -n ingress-nginx -o wide
-fi
-
-# Ingress service
-kubectl apply -f ./ingress-svc.yaml
+kubectl apply -f ./envoy-k8s.yaml
+kubectl apply -f ./api-k8s.yaml
--- destroy.sh
+++ destroy.sh
@@ -2,24 +2,11 @@
 
 set -e
 
-if [ -z "$(which kubectl)" -o -z "$(which helm)" ]; then
-  echo "You must install kubectl and helm tools in order to execute this script"
+if [ -z "$(which kubectl)" ]; then
+  echo "You must install kubectl in order to execute this script"
   exit 1
 fi
 
-# limit range
-kubectl delete -f ./limit-range.yaml
-
 # API objects
-kubectl delete -f ./api-deployment.yaml
-kubectl delete -f ./api-svc.yaml
-
-# Envoy objects
-kubectl delete -f ./envoy-deployment.yaml
-kubectl delete -f ./envoy-svc.yaml
-
-# Nginx ingress
-helm uninstall ingress-nginx -n ingress-nginx
-
-# Ingress service
-kubectl delete -f ./ingress-svc.yaml
+kubectl delete -f ./
+kubectl delete ns envoy
